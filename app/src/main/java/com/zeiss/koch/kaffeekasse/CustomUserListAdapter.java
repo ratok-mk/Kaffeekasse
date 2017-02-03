3fd05aac1d9f2db20a8975ea3e923da1ca7f3d7c
koch <ratok.mk@gmail.com>
2017-02-03 01:25:02 +0100

    added user search

--- app/src/main/java/com/zeiss/koch/kaffeekasse/CustomUserListAdapter.java
+++ app/src/main/java/com/zeiss/koch/kaffeekasse/CustomUserListAdapter.java
@@ -5,8 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -14,24 +17,27 @@ import java.util.List;
  * Created by ogmkoch on 07.11.2016.
  */
 
-public class CustomUserListAdapter extends BaseAdapter {
-    private static List<User> searchArrayList;
+public class CustomUserListAdapter extends BaseAdapter implements Filterable {
+    private static List<User> originalData;
+    private List<User> filteredData;
+    private ItemFilter mFilter = new ItemFilter();
 
     private LayoutInflater mInflater;
     private final SqlDatabaseHelper db;
 
     public CustomUserListAdapter(Context context, List<User> results) {
         db = new SqlDatabaseHelper(context);
-        searchArrayList = results;
+        originalData = results;
+        filteredData = results;
         mInflater = LayoutInflater.from(context);
     }
 
     public int getCount() {
-        return searchArrayList.size();
+        return filteredData.size();
     }
 
     public Object getItem(int position) {
-        return searchArrayList.get(position);
+        return filteredData.get(position);
     }
 
     public long getItemId(int position) {
@@ -51,7 +57,7 @@ public class CustomUserListAdapter extends BaseAdapter {
             holder = (ViewHolder) convertView.getTag();
         }
 
-        User user = searchArrayList.get(position);
+        User user = originalData.get(position);
         Double balance = db.getBalance(user);
         String balanceText = Formater.valueToCurrencyString(balance);
         holder.txtName.setText(String.format("%1$d. %2$s", position + 1, user.getName()));
@@ -72,5 +78,41 @@ public class CustomUserListAdapter extends BaseAdapter {
     static class ViewHolder {
         TextView txtName;
         TextView txtInfo;
+    }
+
+    public Filter getFilter() {
+        return mFilter;
+    }
+
+    private class ItemFilter extends Filter {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+
+            String filterString = constraint.toString().toLowerCase();
+            FilterResults results = new FilterResults();
+            final List<User> list = originalData;
+            int count = list.size();
+            final ArrayList<User> nlist = new ArrayList<User>(count);
+
+            User filterableUser ;
+            for (int i = 0; i < count; i++) {
+                filterableUser = list.get(i);
+                if (filterableUser.getName().toLowerCase().contains(filterString)) {
+                    nlist.add(filterableUser);
+                }
+            }
+
+            results.values = nlist;
+            results.count = nlist.size();
+            return results;
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            filteredData = (ArrayList<User>) results.values;
+            notifyDataSetChanged();
+        }
+
     }
 }
